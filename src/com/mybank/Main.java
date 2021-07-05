867d9cb432f29b58306b405862be5d9fa27c7135
Jesse Mccubbin <jessemccubbin1@gmail.com>
2021-07-05 15:00:42 +1000

    Adding another method call to main

--- src/com/mybank/Main.java
+++ src/com/mybank/Main.java
@@ -10,9 +10,13 @@ public class Main {
 	public static void main(String[] args) {
 		
 //		MusicalChairs.play();
+		
 //		MusicalChairs.playWithCollections();
+		
 //		MusicalChairs.playWithSet();
-//		MusicalChairs.playWithMap();
+		
+		MusicalChairs.playWithMap();
+		
 		MusicalChairs.playWithSorting();
 	}
 }
